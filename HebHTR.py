f70fd843632581c9fa91dbbe061b525120e5c3a8
galgrudkaTau <95225981+galgrudkaTau@users.noreply.github.com>
2023-09-02 14:17:28 +0300

    Update HebHTR.py
    updated the model. now runs on windows. (added .pyd file)
    adjusted methods mainly on in Model.py
    
    added .pyd file

--- HebHTR.py
+++ HebHTR.py
@@ -1,15 +1,18 @@
 from processFunctions import *
 from predictWord import *
 import os
+import matplotlib.pyplot as plt
+
 
 class HebHTR:
 
     def __init__(self, img_path):
         self.img_path = img_path
-        self.original_img = cv2.imread(img_path)
+        self.original_img = plt.imread(fr'{img_path}', cv2.IMREAD_GRAYSCALE)
+        # RGB --> BGR
+        self.original_img = self.original_img[:, :, ::-1].copy()
 
-
-    def imgToWord(self, iterations=5, decoder_type='word_beam'):
+    def imgToWord(self, iterations=5, decoder_type='best_path'):
         transcribed_words = []
         model = getModel(decoder_type=decoder_type)
         transcribed_words.extend(predictWord(self.original_img, model))
